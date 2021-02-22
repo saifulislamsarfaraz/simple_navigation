794a30cc1bb5fcff21a35ba63b17eb3ea86914d3
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-02-22 21:51:57 +0600

    second circle

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
+const openLeft = document.getElementById('openLeft')
+const closeLeft = document.getElementById('closeLeft')
+//const container = document.querySelector('.container')
+
 
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
 
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
+
+closeLeft.addEventListener('click', () => container.classList.remove('show-nav1'))
+
+
+openLeft.addEventListener('click', () => container.classList.add('show-nav1'))
 
 
 
